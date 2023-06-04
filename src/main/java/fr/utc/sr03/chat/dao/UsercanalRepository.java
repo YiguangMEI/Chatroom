9a2ff3d5c484691e12514766fb5fe2c9cc3cda0d
jeunecrayon <yiguang.mei@etu.utc.fr>
2023-06-04 20:37:24 +0200

    myg1

--- src/main/java/fr/utc/sr03/chat/dao/UsercanalRepository.java
+++ src/main/java/fr/utc/sr03/chat/dao/UsercanalRepository.java
@@ -3,9 +3,13 @@ package fr.utc.sr03.chat.dao;
 
 import fr.utc.sr03.chat.model.Usercanal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UsercanalRepository extends JpaRepository<Usercanal,Long>{
     // Requete generee automatiquement par Spring
-    Usercanal findByuserid(@Param("user_id") Long user_id);
+
+    List<Usercanal> findByuser_id(@Param("user_id") Long user_id);
 }
